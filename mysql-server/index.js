10a6372c10e42b8fef2c7c9bac8bbf731d909152
unknown <nare.sakhoyan@gmail.com>
2020-06-30 13:30:20 +0400

    updated sqls

--- mysql-server/index.js
+++ mysql-server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 
 let corsOptions = {
     origin: 'http://localhost:8080'
